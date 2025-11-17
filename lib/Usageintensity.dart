2242b7f706a8eeddcd26dacc606f3c521f754931
SamerMrf13 <samermrf13@gmail.com>
2025-11-17 17:27:23 +0200

    Finished Usageintensity.dart

--- lib/Usageintensity.dart
+++ lib/Usageintensity.dart
@@ -4,27 +4,40 @@ class Usageintensity extends StatefulWidget {
   Battery battery;
   final Function(String) updateUsage;
   Usageintensity({required this.battery,required this.updateUsage,super.key});
-
   @override
   State<Usageintensity> createState() => _UsageintensityState();
 }
-
 class _UsageintensityState extends State<Usageintensity> {
   String usage='Light';
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Row(//Row of Radio buttons
       children: [
         Radio(value:'Light',groupValue:usage,
           onChanged:(val){
-          setState(() {
+          setState(() {//Calls set state in home and updates UI based on user choice
             usage=val as String;
             widget.updateUsage(usage);
           });
           },
         ),
-        Text('Light',style:TextStyle(fontSize:18)),
-
+        Text('Light',style:TextStyle(fontSize:18)),//Text to display choices
+        Radio(value:'Moderate',groupValue:usage,
+        onChanged:(val){
+          setState(() {
+            usage=val as String;
+            widget.updateUsage(usage);
+          });
+        },),
+        Text('Moderate',style:TextStyle(fontSize:18)),
+        Radio(value:'High',groupValue:usage,
+        onChanged:(val){
+          setState(() {
+            usage=val as String;
+            widget.updateUsage(usage);
+          });
+        },),
+        Text('High',style:TextStyle(fontSize:18)),
       ],
     );
   }
